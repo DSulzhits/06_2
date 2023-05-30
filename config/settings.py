769d286e674801f1f82711209a811024f5f91955
DSulzhits <dsulzhits@gmail.com>
2023-05-30 16:16:25 +0300

    Добавлены директории для 'static'

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 MEDIA_URl = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
