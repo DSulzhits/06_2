57c6ea8998151ba327230cc3a179e4f52cd3614e
DSulzhits <dsulzhits@gmail.com>
2023-05-31 10:46:43 +0300

    Скорректировано имя контроллера

--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,7 @@ def home(request):
     return render(request, 'catalog/home.html', context)
 
 
-def display_info(request):
+def info(request):
     return render(request, 'catalog/info.html')
 
 
