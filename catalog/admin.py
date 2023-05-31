5c8b86207e8897b4c5c53fbf9ae1c3b61ef32a99
DSulzhits <dsulzhits@gmail.com>
2023-05-31 10:51:52 +0300

    Внесены корретировки в админку

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,7 @@ from catalog.models import Product, Category
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category',)
+    list_display = ('id', 'name', 'price', 'category', 'image',)
     search_fields = ('name', 'description',)
     list_filter = ('category',)
 
