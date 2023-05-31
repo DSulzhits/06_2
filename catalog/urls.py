d62d19992046d5c6c8c8edf1518044f56866c51b
DSulzhits <dsulzhits@gmail.com>
2023-05-31 10:47:04 +0300

    Добавлен контроллер info

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, info
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home),
     path('contacts/', contacts),
+    path('info/', info)
 ]
