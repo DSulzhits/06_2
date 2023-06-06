70d9699b68754cab542380bc2a4e542868facef5
DSulzhits <dsulzhits@gmail.com>
2023-06-06 15:47:31 +0300

    Убраны ненужные urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, ContactsView, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductListView
+from catalog.views import HomeView, ContactsView, ProductDetailView, ProductListView
 
 app_name = CatalogConfig.name
 
@@ -11,6 +10,4 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
-    path('product/create/', ProductCreateView.as_view(), name='product_create'),
-    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
 ]
