ad8d8e43ab623994dace2d07ba65a669273acc49
DSulzhits <dsulzhits@gmail.com>
2023-06-07 10:56:01 +0300

    Добавлены контроллеры:  BlogRecordListView;  BlogRecordDetailView;  BlogRecordCreateView;  BlogRecordUpdateView;  BlogRecordDeleteView. Для работы с моделью BlogRecord

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from catalog.models import Product
+from catalog.models import Product, BlogRecord
 from django.views import generic
+from django.urls import reverse_lazy
 
 
 class HomeView(generic.TemplateView):
@@ -57,6 +58,7 @@ class ContactsView(generic.TemplateView):
         print(f'User {name}, with phone {phone}, send message: {message}')
         return render(request, self.template_name)
 
+
 # def contacts(request):
 #    if request.method == "POST":
 #        name = request.POST.get('name')
@@ -64,3 +66,41 @@ class ContactsView(generic.TemplateView):
 #        message = request.POST.get('message')
 #        print(f'User {name}, with phone {phone}, send message: {message}')
 #    return render(request, 'catalog/contacts.html')
+
+class BlogRecordListView(generic.ListView):
+    model = BlogRecord
+    extra_context = {
+        'title': 'Список записей'
+    }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_active=True)
+        return queryset
+
+
+class BlogRecordDetailView(generic.DetailView):
+    model = BlogRecord
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        # context_data['title'] = context_data['object']
+        context_data['title'] = self.get_object()
+        return context_data
+
+
+class BlogRecordCreateView(generic.CreateView):
+    model = BlogRecord
+    fields = ('title', 'slug', 'content', 'preview')
+    success_url = reverse_lazy('catalog: blog_records_list')
+
+
+class BlogRecordUpdateView(generic.UpdateView):
+    model = BlogRecord
+    fields = ('title', 'slug', 'content', 'preview')
+    success_url = reverse_lazy('catalog: blog_records_list')
+
+
+class BlogRecordDeleteView(generic.DeleteView):
+    model = BlogRecord
+    success_url = reverse_lazy('catalog: blog_records_list')
