988fef4a0b6f64cdbba9369b868bf0df3f2ae8ef
DSulzhits <dsulzhits@gmail.com>
2023-06-14 10:50:23 +0300

    Создан файл forms.py, написаны первоначальные формы ProductForm, BlogRecordForm

---
+++ catalog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from catalog.models import Product, BlogRecord
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class BlogRecordForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = BlogRecord
+        fields = ('title', 'slug', 'content', 'preview')
