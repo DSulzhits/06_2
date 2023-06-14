a65145f9437e7baff78f4136e9896f58b06b81ac
DSulzhits <dsulzhits@gmail.com>
2023-06-14 13:19:22 +0300

    Добавлены urls исходя из нового функционала

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, ContactsView, ProductDetailView, ProductListView, BlogRecordListView, \
+from catalog.views import HomeView, ContactsView, ProductDetailView, ProductListView, ProductDeleteView, \
+    ProductCreateView, \
+    ProductUpdateView, BlogRecordListView, \
     BlogRecordDetailView, BlogRecordCreateView, BlogRecordUpdateView, BlogRecordDeleteView, \
     BlogRecordDeactivatedListView, toggle_activity
 
@@ -11,7 +13,10 @@ urlpatterns = [
     path('', HomeView.as_view(), name='homepage'),
     path('products/', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
     path('blog_records/', BlogRecordListView.as_view(), name='blog_records'),
     path('blog_records_deactivated/', BlogRecordDeactivatedListView.as_view(), name='blog_records_deactivated'),
     path('blog_records/<slug:slug>/', BlogRecordDetailView.as_view(), name='blog_record_detail'),
