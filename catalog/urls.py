ca0e1b285f9e6539571ad7041f308d6b04809189
DSulzhits <dsulzhits@gmail.com>
2023-06-16 17:16:18 +0300

    Добавлена urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ from catalog.views import HomeView, ContactsView, ProductDetailView, ProductList
     ProductCreateView, \
     ProductUpdateView, BlogRecordListView, \
     BlogRecordDetailView, BlogRecordCreateView, BlogRecordUpdateView, BlogRecordDeleteView, \
-    BlogRecordDeactivatedListView, toggle_activity, VersionDetailView
+    BlogRecordDeactivatedListView, toggle_activity, VersionDetailView, VersionListView, VersionCreateView
 
 app_name = CatalogConfig.name
 
@@ -17,7 +17,8 @@ urlpatterns = [
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
-    path('version/<int:pk>', VersionDetailView.as_view(), name='product_version'),
+    path('version/create/', VersionCreateView.as_view(), name='version_create'),
+    path('version/<int:pk>/', VersionDetailView.as_view(), name='product_version'),
     path('blog_records/', BlogRecordListView.as_view(), name='blog_records'),
     path('blog_records_deactivated/', BlogRecordDeactivatedListView.as_view(), name='blog_records_deactivated'),
     path('blog_records/<slug:slug>/', BlogRecordDetailView.as_view(), name='blog_record_detail'),
