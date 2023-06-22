e761efdd4622a1adb782fa10dc16790f6ed83f25
DSulzhits <dsulzhits@gmail.com>
2023-06-22 13:09:47 +0300

    Скорректированы отступы

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from catalog.forms import StyleFormMixin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django import forms
+from catalog.forms import StyleFormMixin
 from users.models import User
 
 
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
+        fields = ('email', 'password1', 'password2',)
 
 
 class UserForm(StyleFormMixin, UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name', 'last_name', 'phone', 'avatar', 'country')
+        fields = ('email', 'password', 'first_name', 'last_name', 'phone', 'avatar', 'country',)
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-            self.fields['password'].widget = forms.HiddenInput()
+        self.fields['password'].widget = forms.HiddenInput()
+        print(self.fields['password'])
