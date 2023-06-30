d34d07989f2fefb7e0019030fe7f6031fb409a02
DSulzhits <dsulzhits@gmail.com>
2023-06-30 20:00:11 +0300

    Адрес почты перенесен в окружение

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ if CACHE_ENABLED:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'dmitrysulzhits@yandex.by'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('YANDEX_PASSWORD_APP')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
