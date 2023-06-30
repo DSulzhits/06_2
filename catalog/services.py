0bbc64592a6064161ccb1f2edaf39e5a992e2534
DSulzhits <dsulzhits@gmail.com>
2023-06-30 19:24:25 +0300

    Прописана возможность кеширования объектов модели Category

--- catalog/services.py
+++ catalog/services.py
@@ -1,5 +1,7 @@
 from django.core.mail import send_mail
 from django.conf import settings
+from django.core.cache import cache
+from catalog.models import Category
 
 
 def send_email(record_object):
@@ -9,3 +11,15 @@ def send_email(record_object):
         settings.EMAIL_HOST_USER,
         recipient_list=['dsulzhits@gmail.com', 'suz17@bk.ru']
     )
+
+
+def get_cached_category_subjects():
+    if settings.CACHE_ENABLED:
+        key = 'category_list'
+        category_list = cache.get(key)
+        if category_list is None:
+            category_list = Category.objects.all()
+            cache.set(key, category_list)
+    else:
+        category_list = Category.objects.all()
+    return category_list
